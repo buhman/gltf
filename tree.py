4547fa6e9f33164791b974410862cc540943f847
Zack Buhman <zack@buhman.org>
2026-01-04 23:28:15 -0600

    tree: check joint parent order

--- tree.py
+++ tree.py
@@ -7,9 +7,15 @@ def build_tree(gltf):
         if "children" not in node:
             continue
         for child_ix in node["children"]:
-            assert child_ix > node_ix, (child_ix, node_ix)
             assert child_ix not in parents
             parents[child_ix] = node_ix
+
+    for skin in gltf.json["skins"]:
+        seen = set()
+        for joint in skin["joints"]:
+            parent = parents[joint]
+            assert parent in seen or parent not in skin["joints"], (parent, joint, seen)
+            seen.add(joint)
 
     return parents
 
